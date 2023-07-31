c4947ccf55270cd875f79c5014fc1ace0c6a9011
Divya Mehrotra <98140177+divyamehrotra@users.noreply.github.com>
2023-07-31 11:19:24 +0530

    Added Solution - GfG to GitHub

---
+++ School/Print alternate elements of an array/print-alternate-elements-of-an-array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// ar[] is the array 
+// n is the number of elements in array.
+void print(int ar[], int n)
+{
+    if(n%2==0){
+        for(int i=0;i<n/2;i++){
+            cout<<ar[i*2]<<" ";
+        }
+    }
+    else{
+        for(int i=0;i<n/2+1;i++){
+            cout<<ar[2*i]<<" ";
+        }
+    }
+    
+}
+
+//{ Driver Code Starts.
+int main()
+{
+   int t;
+   cin>>t;
+  while(t--)
+   {
+     int ar[100001]={0};
+     int n;
+     cin>>n;
+     for(int i=0;i<n;i++)
+      cin>>ar[i];
+      print(ar,n);
+      cout<<endl;
+     }
+
+return 0;
+}
+
+// } Driver Code Ends
