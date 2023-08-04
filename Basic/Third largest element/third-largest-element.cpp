6819f7be5b6d14365a199fb9b3bc19beb343a3a8
Divya Mehrotra <98140177+divyamehrotra@users.noreply.github.com>
2023-08-04 23:43:13 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Third largest element/third-largest-element.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int thirdLargest(int a[], int n)
+    {
+        //Your code here
+        sort(a,a+n);
+        int max=INT_MIN;
+        for(int i = 0; i < n-2; i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        return max;
+    }
+
+};
+
+//{ Driver Code Starts.
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution obj;
+	    cout<<obj.thirdLargest(a,n)<<endl;
+    }
+}   
+// } Driver Code Ends
